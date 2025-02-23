3b20280ca8224fb3336735a91b87e29edd61e81e
BabyKaban <paul92219@proton.me>
2025-02-22 19:45:49 -0800

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route, Redirect } from "react-router-dom";
+
+import Home from "./pages/Home.jsx";
+import About from "./pages/About.jsx";
+import Contact from "./pages/Contact.jsx";
+
+import BlogPost from './components/BlogPost/BlogPost';
+
+import { AnimatePresence } from "framer-motion";
+import { GlobalStyle } from "./components/StyledComponents/StyledComponents.jsx";
+import { ThemeProvider } from "styled-components";
+import theme from "./components/StyledComponents/Theme";
+
+import Posts from '../src/posts/Posts';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <BrowserRouter>
+        <AnimatePresence>
+          <Routes>
+            <Route path="/home" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+           
+            {Posts.map(post => (
+                <Route path={post.route} 
+                element={
+                <BlogPost title={post.title}
+                          date={post.date}
+                          image={post.image}
+                          content={post.content} />} />
+            ))}
+          </Routes>
+        </AnimatePresence>
+      </BrowserRouter>
+    </ThemeProvider >
+  </React.StrictMode>
+);
